a73df0cf20e1c9da676a92148808fe5f6b95a784
Misigomartin <Misigomartin@gmail.com>
2022-06-12 20:49:26 +0300

    create serializer  file

---
+++ awardapp/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import *
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Project
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields = '__all__'
+        
+class VotesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Votes
+        fields = '__all__'
+
+
+
+
+
+
+
+
+
+
+
