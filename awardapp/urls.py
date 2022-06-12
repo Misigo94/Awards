8c3941e5c1e4ad0969786ce1b26761583a3215bf
Misigomartin <Misigomartin@gmail.com>
2022-06-12 20:46:46 +0300

    create urls  file

---
+++ awardapp/urls.py
@@ -0,0 +1,19 @@
+from awardapp import views
+from django.urls import path
+from .views import *
+
+
+urlpatterns=[
+    path('profile/',profile_list,name='profile'),
+    path('profile/<int:id>',profile_detail),
+    path('display_profile/',display_profile),
+    path('',views.index, name='home'),
+    path('register/',register_user,name='register'),
+    path('display_project/',display_project),
+    path('votes/',dis_votes),
+    path('login/',views.login_user,name='login'),
+    path('logout/',views.login_user,name='logout'),
+    path('search/',views.search,name='search'),
+    path('project/',project_list,name="project"),
+    path('display_project/',display_project),
+]
