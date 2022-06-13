df35b09421bdd98de6ce40f094321f2696271775
Misigomartin <Misigomartin@gmail.com>
2022-06-13 20:50:11 +0300

    added urls file

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -6,14 +6,15 @@ from .views import *
 urlpatterns=[
     path('profile/',profile_list,name='profile'),
     path('profile/<int:id>',profile_detail),
-    path('display_profile/',display_profile),
+    path('display_profile/',display_profile,name='profileform'),
     path('',views.index, name='home'),
     path('register/',register_user,name='register'),
     path('display_project/',display_project),
-    path('votes/',dis_votes),
+    path('votes/',dis_votes,name='votes'),
     path('login/',views.login_user,name='login'),
     path('logout/',views.login_user,name='logout'),
     path('search/',views.search,name='search'),
     path('project/',project_list,name="project"),
-    path('display_project/',display_project),
+    path('display_project/',display_project,name='projectform'),
+    path('output/',views.output,name='output'),
 ]
