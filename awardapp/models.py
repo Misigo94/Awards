052e5e411a8c29217c787747d51c60a821025ae7
Misigomartin <Misigomartin@gmail.com>
2022-06-14 11:44:31 +0300

    created a new attribute to votes model

--- awardapp/models.py
+++ awardapp/models.py
@@ -16,17 +16,39 @@ class Project(models.Model):
     link = models.URLField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     profile = models.ManyToManyField(Profile)
+
     def __str__(self):
         return self.title
 
 class Votes(models.Model):
-    design= models.IntegerField(default=0)
-    usability= models.IntegerField(default=0)
-    content = models.IntegerField(default=0)
-
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE, null=True, blank=True)
+    RATING_CHOICES = (
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+        (5,'5'),
+        (6,'6'),
+        (7,'7'),
+        (8,'8'),
+        (9,'9'),
+        (10,'10'),
+    )
+    design=models.IntegerField(choices=RATING_CHOICES,blank=True,null=True)
+    usability=models.IntegerField(choices=RATING_CHOICES,blank=True,null=True)
+    content= models.IntegerField(choices=RATING_CHOICES,blank=True,null=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,related_name='profile',null=True,blank=True)
     project = models.ForeignKey(Project, on_delete=models.CASCADE,null=True, blank=True)
 
+    def save_review(self):
+        self.save()
+    def delete_review(self):
+        self.delete()
+    def __str__(self):
+        return self.profile.username
+
+
+     
+   
 class Login(models.Model):
     username =  models.CharField(max_length=255)
     password =  models.CharField(max_length=30)
